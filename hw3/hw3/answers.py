2c5ad016716eb8a206eea932f64e995557f84d52
Ofir Nesher <Nesher123@gmail.com>
2021-12-18 14:24:03 +0200

    answered the open questions

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -5,6 +5,7 @@ Note: Inside the answer strings you can use Markdown format and also LaTeX
 math (delimited with $$).
 """
 
+
 # ==============
 # Part 1 answers
 
@@ -32,25 +33,53 @@ def part1_generation_params():
     temperature = 0.0001
     # TODO: Tweak the parameters to generate a literary masterpiece.
     # ====== YOUR CODE: ======
-
+    # TODO: Meital, I just put something here but change it if it doesn't do anything
+    start_seq = "Dear"
+    temperature = 0.1
     # ========================
     return start_seq, temperature
 
 
+# Why do we split the corpus into sequences instead of training on the whole text?
 part1_q1 = r"""
-**Your answer:**
+We split the corpus into sequences instead of training on the whole text because:<br>
+a. The learning process is more generalized this way, and prevents overfitting 
+and can improve the robustness of model when available training data is limited.<br>
+b. It might be too large to fit in memory and result in very long training times 
+(see https://arxiv.org/ftp/arxiv/papers/1708/1708.05604.pdf).<br>
+&emsp; More troubling, attempting to back-propagate across very long input sequences may result in vanishing gradients, 
+and in turn, an un-learnable model.
 """
 
+# How is it possible that the generated text clearly shows memory longer than the sequence length?
 part1_q2 = r"""
-**Your answer:**
+The generated text shows memory longer than the sequence length because of the hidden states ,H 
+("This can be thought of as the memory of that layer").<br>
+These pass previous knowledge and therefore have more memory in each layer.
 """
 
+# Why are we not shuffling the order of batches when training?
 part1_q3 = r"""
-**Your answer:**
+We are NOT shuffling the order of batches when training because each batch rely on its previous batch's hidden state 
+(the "memory").<br>
+Naturally, the continuity of text is important for context and logic, and if we shuffle words we can understand the text 
+differently (or not understand at all)...<br>
+Here, "understanding" means generating similar texts, so we learn the text as it is, otherwise the resulted text will 
+be irrelevant.
 """
 
+# 1. Why do we lower the temperature for sampling (compared to the default of 1.0)?
+# 2. What happens when the temperature is very high and why?
+# 3. What happens when the temperature is very low and why?
 part1_q4 = r"""
-**Your answer:**
-"""
+1. When sampling, we would prefer to control the distributions and make them less uniform to increase the chance of 
+sampling the char(s) with the <u>highest scores</u> compared to the others.<br>
+Low temperature results in less uniform distributions and more structured sampling (which we want).<br>
+<br>
+2. When the temperature is very high the probabilities are more uniform and the model will therefore choose random 
+chars for each word, and the generated text might have no meaning...<br>
+<br>
+3. When the temperature is very low the probabilities are not uniform at all and the model will therefore always choose 
+the chars that are more common (more "safe" to choose) from training.<br>
+This result in less generalized generated text and thus less errors but more similar outputs all the time."""
 # ==============
-
