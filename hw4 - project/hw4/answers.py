e150a149595e0bc51af81988c6f40bdbc928625c
OfirNesher <Nesher123@gmail.com>
2022-02-06 23:56:13 +0200

    answered open question of GAN part

--- hw4 - project/hw4/answers.py
+++ hw4 - project/hw4/answers.py
@@ -141,19 +141,68 @@ def part3_gan_hyperparams():
     return hypers
 
 
+# Explain in detail why during training we sometimes need to maintain gradients when sampling from the GAN,
+# and other times we don’t.
+# When are they maintained and why? When are they discarded and why?
 part3_q1 = r"""
-**Your answer:**
+The GAN consists of two models: Discriminator & Generator.<br>
+Therefore, we also split the training parts since we dont want one training to affect the other.
 
+The reason for discarding the gradients in the discriminator training is that we dont want the discriminator 
+training to affect the generator training.<br>
+We want the generator gradients to be affected only by its own loss, and to not update them during discriminator train.
+
+While optimizing the discriminator we're adding some fake inputs to the original inputs.<br>
+Both inputs create a loss, but we want to maintain the discriminator' gradients solely according to the original inputs.
+<br>
+The fake data is only used for testing the performance and we don't want to update the discriminator accordingly.
 """
 
+# 1. When training a GAN to generate images, should we decide to stop training solely based on the fact that the
+# Generator loss is below some threshold? Why or why not?
+# 2. What does it mean if the discriminator loss remains at a constant value while the generator loss decreases?
 part3_q2 = r"""
-**Your answer:**
+**1**.<br>
+When training a GAN to generate images, we should **NOT** decide to stop training solely based on the fact that the 
+Generator's loss is below some threshold because our eventual goal is for the generator to be good enough so that the 
+discriminator would not be able to differentiate between a real and a fake image.<br> 
+In case we stop solely based on a threshold, we make the discriminator's job easier and it might be able to 
+differentiate, so we stopped prematurely.<br>
 
+Since the Generator's training may take quite some time, the early stopping according to the generator's loss alone is 
+not recommended.
+
+We should bare in mind that the performance of the GAN depends on the 2 parts (Generator AND Discriminator), so looking 
+only at the generator's performance is clearly not enough.
+
+
+**2**.<br>
+When the discriminator loss remains at a constant value while the generator loss decreases it means that the generator 
+is getting better at "fooling" the discriminator by making more realistic images.<br>
+The fact that the discriminator loss remains at a constant value actually means its classification is ALSO getting 
+better at detecting fake images, but not at the the same velocity as the generator.
 """
 
+# Compare the results you got when generating images with the VAE to the GAN results.
+# What’s the main difference and what’s causing it?
 part3_q3 = r"""
-**Your answer:**
+We saw that <u>GANs are better at generating (more realistic) images</u>.<br>
+Furthermore, during the encoding process of the VAEs, important data is lost naturally, by removing and compressing 
+parts of it, and therefore it yields inferior results compared to the GAN.
 
+The 2 methods have different approaches as well:
+The VAE learning approach is to compress the data correctly in order to be able to reconstruct it later, meaning it 
+focuses directly on the data.
+The GAN approach is to train an entity which is able to distinguish between real and fake data, and another, con-artist 
+entity.<br>
+It "cares" relies more about the two adversaries' expertise, and not on the features of the data.
+
+The background parts in the VAE are more blurry and the face area is more detailed, while the GAN has much more 
+similarities in the background parts.<br>
+
+The differences between the methods are also related to their training process:
+VAEs have a final target (improve accuracy), while GANs' training process is more complicated - 2 models in a 
+competition to "ruin" the other's progress.
 """
 
 # ==============
